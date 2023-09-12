3fcc54ea6be5cc1f5e8400e2fd1bb714659064da
Luís Miguel Torres <94331066+luistorres109@users.noreply.github.com>
2023-09-12 15:46:35 -0300

    Update repeated_code.py

--- anes/functions/repeated_code.py
+++ anes/functions/repeated_code.py
@@ -33,3 +33,33 @@ def extrair_indices(indexes):
     indexes = indexes.split(',')
 
     return indexes
+
+#---------------------------------------------------------------------------------------------------------
+# Função que extrai dataframes, com base nos indices
+def extrair_dataframes(indexes, df):
+    linhas = []
+    # Lista que conterá sub-listas com os dados extraídos do dataframe para formar o novo dataframe
+
+    for index in indexes:
+        # Extrai os dados de cada linha, um por um
+        data = df.iloc[int(index)]['Data']
+        horario = df.iloc[int(index)]['Horario']
+        edital = df.iloc[int(index)]['Edital']
+        local = df.iloc[int(index)]['Local']
+        modalidade = df.iloc[int(index)]['Modalidade']
+        julgamento = df.iloc[int(index)]['Forma julgamento']
+        gestora = df.iloc[int(index)]['Unidade gestora']
+        secretaria = df.iloc[int(index)]['Secretária']
+        objeto = df.iloc[int(index)]['Objeto']
+        montante = df.iloc[int(index)]['Montante']
+        tipo = df.iloc[int(index)]['Tipo']
+        link = df.iloc[int(index)]['Link da Página']
+
+        # Coloca-os em uma sub-lista
+        linha = [data, horario, edital, local, modalidade, julgamento, gestora, secretaria, objeto, montante, tipo, link]
+        linhas.append(linha)
+
+    # Cria um Dataframe com os dados
+    result_df = pd.DataFrame(linhas, columns=['Data', 'Horario', 'Edital', 'Local', 'Modalidade', 'Forma julgamento', 'Unidade gestora', 'Secretária', 'Objeto', 'Montante', 'Tipo', 'Link da Página'])
+
+    return result_df
